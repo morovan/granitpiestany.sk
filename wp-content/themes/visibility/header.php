ecb458fa06df0290edd3d369a4683ad7be3ec2d1
rbalogh <balogh@visibility.sk>
2016-02-08 15:52:41 +0100

    Aa Aa Aa Be Be Be De De De

--- wp-content/themes/visibility/header.php
+++ wp-content/themes/visibility/header.php
@@ -145,8 +145,7 @@ endwhile;endif;wp_reset_query();
     <div class="alt-navbar-slider-wrap" id="slide3"></div>
     <div class="alt-navbar-slider-wrap" id="slide4"></div>
     <div class="alt-navbar-slider-wrap" id="slide5"></div>
-    <h2>DeDeDe</h2>
-    <h2>CCC</h2>
+    <h2>DeDeDeaaasssddd</h2>
   <?php } ?></div>
 </header>
 <article>
